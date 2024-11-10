fc15ab99a3c1c8143cbce4180521eddaa6b4524b
FabricioSalustiano <fabriciosalustiano1@gmail.com>
2024-11-10 14:24:09 -0300

    feat: Mudança de biblioteca dos alertas.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-let totalTime = 5000;
-let barraProgresso = document.getElementById('barraProgresso');
-let alerta = document.getElementById('alerta');
-let textoAlerta = document.getElementById('textoAlerta');
-let mensagemAlerta = document.getElementById('mensagemAlerta');
+const alertConfigs = {
+    grave: {
+        classe: 'alert_danger',
+        mensagem: '<strong>URGENTE</strong>: Vítima em possível perigo de morte. Acione emergência!'
+    },
+    medio: {
+        classe: 'alert_warning',
+        mensagem: '<strong>ATENÇÃO</strong>: Verifique com atenção. A situação pode exigir acompanhamento.'
+    },
+    baixo: {
+        classe: 'alert_info',
+        mensagem: '<strong>ATENÇÃO</strong>: Acompanhe o caso com cautela. Não há urgência imediata.'
+    }
+};
 
-let width = 0;
-let interval;
+let alerta = document.getElementById('alerta');
+let mensagemAlerta = document.getElementById('mensagem-alerta');
 
 function cadastrarOcorrencia(event) {
     event.preventDefault();
 
     setAlert('grave');
 
-    
-    const btnRegistrar = document.getElementById("btnRegistrar");
-    const spinner = document.getElementById("spinner");
     const idade = document.getElementById('idade').value;
     const profissao = document.getElementById('profissao').value;
     const relacao_agressor = document.getElementById('relacao_agressor').value;
@@ -28,8 +34,6 @@ function cadastrarOcorrencia(event) {
         'Gênero_do_suspeito': genero,
         'Histórico_de_violência': historico_violencia
     };
-
-    habilitarLoader(btnRegistrar, spinner);
     
     fetch('/predict', {
         method: 'POST',
@@ -56,85 +60,18 @@ function cadastrarOcorrencia(event) {
     .catch(error => {
         console.error('Erro:', error);
     });
-
-    setTimeout(function() {
-        desabilitarLoader(btnRegistrar, spinner);
-    }, 1000);
 }
 
-function habilitarLoader(button, spinner) {
-    button.disabled = true;
-    spinner.style.display = "inline-block";
-}
-
-function desabilitarLoader(button, spinner) {
-    button.disabled = false;
-    spinner.style.display = "none";
+function limparClassesDeAlerta() {
+    alerta.classList.remove('alert_danger', 'alert_warning', 'alert_info', 'alert_none');
 }
 
 function setAlert(gravidade) {
-    let icon = document.querySelector('#alerta svg use');
-
-    limparAlerta();
-
-    switch (gravidade) {
-        case 'grave':
-            icon.setAttribute('xlink:href', '#exclamation-triangle-fill');
-            alerta.classList.add('alert-danger');
-            barraProgresso.classList.add('bg-danger');
-            textoAlerta.innerHTML = 'URGENTE:';
-            mensagemAlerta.innerHTML = 'Vítima em possivel perigo de morte. Acione emergência!'
-            break;
-        case 'medio':
-            icon.setAttribute('xlink:href', '#info-fill');
-            alerta.classList.add('alert-warning');
-            barraProgresso.classList.add('bg-warning');
-            textoAlerta.innerHTML = 'ATENÇÃO:';
-            mensagemAlerta.innerHTML = 'Verifique com atenção. A situação pode exigir acompanhamento.';
-            break;
-        case 'baixo':
-            icon.setAttribute('xlink:href', '#info-fill');
-            alerta.classList.add('alert-info');
-            barraProgresso.classList.add('bg-info');
-            textoAlerta.innerHTML = 'ATENÇÃO:';
-            mensagemAlerta.innerHTML = 'Acompanhe o caso com cautela. Não há urgência imediata.';
-            break;
-        default:
-            break;
+    limparClassesDeAlerta();
+    
+    const config = alertConfigs[gravidade];
+    if (config) {
+        alerta.classList.add(config.classe);
+        mensagemAlerta.innerHTML = config.mensagem;
     }
-
-    width = 0;
-    barraProgresso.style.width = width + '%';
-    barraProgresso.setAttribute('aria-valuenow', width);
-
-    alerta.classList.add('show');
-
-    habilitarBarraProgresso();
 }
-
-function habilitarBarraProgresso() {
-    interval = setInterval(function() {
-        width += 2;
-        barraProgresso.style.width = width + '%';
-        barraProgresso.setAttribute('aria-valuenow', width);
-        
-        if (width >= 100) {
-            clearInterval(interval);
-            alerta.classList.remove('show');
-        }
-    }, totalTime / 50);
-}
-
-function limparAlerta() {
-    alerta.classList.remove('show');
-    alerta.classList.remove('alert-danger', 'alert-warning', 'alert-info');
-    barraProgresso.classList.remove('bg-danger', 'bg-warning', 'bg-info');
-
-    textoAlerta.innerHTML = '';
-    mensagemAlerta.innerHTML = '';
-}
-
-document.querySelector('.btn-close').addEventListener('click', function() {
-    limparAlerta();
-    clearInterval(interval);
-});
